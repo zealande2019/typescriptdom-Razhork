601931296f6b33a40491c59b5a2d98bb2961a98f
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-04-02 21:18:20 +0200

    hey

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,22 @@
-//Her skal du skrive din typescriptkode
+/* OPGAVE 1
+let var1 = document.getElementById("2");
+console.log(var1)
+
+let var2 = document.getElementsByClassName("sundt")
+console.log(var2)
+
+let var3 = document.getElementsByTagName("li")
+console.log(var3)
+*/
+
+
+/* OPGAVE 3
+document.getElementById("3").setAttribute("class", "roflmao")
+document.getElementById("2").hidden = true;
+document.getElementsByClassName("usundt")[0].setAttribute("class", "sundt")
+*/
+
+
+let var4 = document.getElementById("liste").firstChild.nodeName;
+console.log(var4)
+
